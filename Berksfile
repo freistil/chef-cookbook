cec7f6f946f4c8ab85416af78c9088d46ef09bd3
Jochen Lillich <jochen@lillich.info>
2013-07-29 16:14:29 +0200

    Initial commit

---
+++ Berksfile
@@ -0,0 +1,12 @@
+site :opscode
+
+cookbook 'apache2',
+  git: 'ssh://git@bitbucket.org/freistil/chef-apache2.git'
+
+cookbook 'apache2-freistil',
+  git: 'ssh://git@bitbucket.org/freistil/chef-apache2-freistil.git'
+
+cookbook 'incron',
+  git: 'ssh://git@bitbucket.org/freistil/chef-incron.git'
+
+metadata
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-cookbook'
+maintainer       'freistil IT'
+maintainer_email 'cookbooks@freistil.it'
+license          'Apache 2.0'
+description      'Installs/Configures chef-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
